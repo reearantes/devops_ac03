277747f42d0aec53f6b7b6bad75f17493a183b44
ree arantes <renae.lourenco@aluno.faculdadeimpacta.com.br>
2020-10-07 23:24:04 -0300

    incluindo arquivo de teste projeto

--- com/renae/test/test_operacoes.py
+++ com/renae/test/test_operacoes.py
@@ -1,11 +1,6 @@
-def mul(x,y):
-	return x*y
-
+def media_movel(d1,d2,d3,d4,d5,d6,d7):
+	return (d1+d2+d3+d4+d5+d6+d7)/7
 def test_valor():
-	assert mul(5,5) == 25
-
-def test_negativo():
-	assert mul(2,-2) == -4
-
+	assert media_movel(1400,1400,2800,1414,714,1421,721) == 1410, "media movel da semana" 
 def test_zero():
-	assert mul(0,0) == 0
+	assert media_movel(0,0,0,0,0,0,0) == 0, "media movel zero"
