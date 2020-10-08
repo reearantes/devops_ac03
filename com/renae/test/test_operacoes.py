0ea15575e068351145b9441c7761e99b629098f3
ree arantes <renae.lourenco@aluno.faculdadeimpacta.com.br>
2020-10-07 23:59:15 -0300

    arquivo de teste projeto integrado

--- com/renae/test/test_operacoes.py
+++ com/renae/test/test_operacoes.py
@@ -1,5 +1,5 @@
 def media_movel(d1,d2,d3,d4,d5,d6,d7):
-	return (d1+d2+d3+d4+d5+d6+d7)/6
+	return (d1+d2+d3+d4+d5+d6+d7)/7
 def test_valor():
 	assert media_movel(1400,1400,2800,1414,714,1421,721) == 1410, "media movel da semana" 
 def test_zero():
